0a596360ac626ad02879a5a37943dcde283093c4
John Cokos <highlander.44@gmail.com>
2019-04-18 09:50:27 -0700

    better game play

--- src/api/v1.events.js
+++ src/api/v1.events.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getChars = (num) => {
-  return [...Array(num)].map(i=>(~~(Math.random()*36)).toString(36));
+  return [...Array(num)].map(i=>(~~(Math.random() * 36)).toString(36));
 };
 
 const events = io => {
@@ -11,17 +11,34 @@ const events = io => {
     console.log(`Socket connected with id ${socket.id}...`);
     console.log('Your app is connected');
 
-    let chars = getChars(3);
+    let num = 1;
+    let wins = 0;
+
+    let chars = getChars(num);
     socket.emit('output', `Quick ... type these characters: ${chars.join('  ')}`);
 
     socket.on('input', payload => {
 
       const correct = payload === chars.join('');
 
-      socket.emit('output', correct ? 'CORRECT!' : 'Nope, Try Again');
+      let message = '';
 
-      chars = getChars(3);
-      socket.emit('output', `Quick ... type these characters: ${chars.join('  ')}`);
+      if ( correct ) {
+        message = 'CORRECT!';
+        num++;
+        wins++;
+      }
+      else {
+        message =  `You Lose, but you had ${wins} Correct Guesses!`;
+        num = 1;
+        wins = 0;
+      }
+
+      socket.emit('output', message);
+
+      chars = getChars( num );
+
+      socket.emit('output', `GO: ${chars.join(' ')}`);
 
     });
     socket.on('disconnect', () => console.log(`Goodbye client: ${socket.id}`));
